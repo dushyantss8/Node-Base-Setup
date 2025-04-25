dbe25e5a20107aa434c45b932df4a397ac88b8aa
Dushyant Vishwakarma <dushyantss8@gmail.com>
2025-04-25 09:08:54 +0530

    Initial Project Setup.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, {Express, Request, Response} from "express";
+import { PORT } from "./secrets";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Working...");
+});
+
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`);
+});
