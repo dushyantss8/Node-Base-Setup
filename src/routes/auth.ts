7d527c10820da9b7e3909a282004c79188f8ae97
Dushyant Vishwakarma <dushyantss8@gmail.com>
2025-04-26 22:24:39 +0530

    Add essential folders, fix prisma directories, add signup route.

---
+++ src/routes/auth.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { signUp }  from '../controllers/auth';
+
+const authRoutes:Router = Router();
+
+authRoutes.post('/sign-up', signUp);
+
+export default authRoutes;
